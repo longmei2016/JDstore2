96c724df604a76c7b1b19684097d00a8a2708fa6
ankon <ankon@ankon-mac.local>
2017-11-03 07:08:13 +0800

    add orders show function

--- app/controllers/cart_items_controller.rb
+++ app/controllers/cart_items_controller.rb
@@ -16,8 +16,9 @@ class CartItemsController < ApplicationController
     if @cart_item.product.quantity >= cart_item_params[:quantity].to_i
       @cart_item.update(cart_item_params)
       flash[:notice] = '成功变更数量'
+    else
+      flash[:notice] = '数量不足以加入购物车'
     end
-    flash[:notice] = '数量不足以加入购物车'
     redirect_to carts_path
   end
 
--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -10,17 +10,17 @@ class ProductsController < ApplicationController
   def add_to_cart
     @product = Product.find(params[:id])
     # 与教材相比有改动-start1
-    @cart = current_cart
-    @cart_item = @cart.cart_items.find_by(product_id: params[:id])
+    # @cart = current_cart
+    # @cart_item = @cart.cart_items.find_by(product_id: params[:id])
     # 与教材相比有改动-end1
     if !current_cart.products.include?(@product)
       current_cart.add_product_to_cart(@product)
       flash[:notice] = "你已成功将 #{@product.title} 加入购物车"
     else
       # 与教材相比有改动-start2
-      @cart_item.quantity += 1
-      @cart_item.save
-      flash[:warning] = '购物车内此物品数量加1'
+      # @cart_item.quantity += 1
+      # @cart_item.save
+      flash[:warning] = '购物车内已有此物品 or 数量加1'
       # 与教材相比有改动-end2
     end
     redirect_to :back
